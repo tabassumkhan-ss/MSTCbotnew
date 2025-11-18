17ed3380a85361f99d8238a1476a4152db56da9a
tabassumkhan-ss <tabbasum.ss@gmail.com>
2025-11-18 11:56:07 +0530

    Update check_ref.py

--- check_ref.py
+++ check_ref.py
@@ -1,2 +1,2 @@
 import sqlite3 
-con=sqlite3.connect("data/betzybot.db");cur=con.cursor();cur.execute("SELECT id, username, referrer_id FROM users WHERE id=2001;");print(cur.fetchone());con.close() 
+con=sqlite3.connect("data/mstcbot.db");cur=con.cursor();cur.execute("SELECT id, username, referrer_id FROM users WHERE id=2001;");print(cur.fetchone());con.close() 
