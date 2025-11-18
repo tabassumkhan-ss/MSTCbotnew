16ddb8065560fe99e27faeb6d5d735075387e5fa
tabassumkhan-ss <tabbasum.ss@gmail.com>
2025-11-18 12:03:56 +0530

    Update view_user.py

--- view_user.py
+++ view_user.py
@@ -1,5 +1,5 @@
 import sqlite3 
-con = sqlite3.connect("data/betzybot.db") 
+con = sqlite3.connect("data/mstcbot.db") 
 cur = con.cursor() 
 cur.execute("SELECT id, username, role, self_activated, total_team_business, active_origin_count, balance_musd FROM users WHERE id=2001;") 
 print(cur.fetchone()) 
